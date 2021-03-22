c3db4a96f9492e221fd175e70cbd92c80bf426eb
sandeep jaiswar <jaiswarsandeep119@gmail.com>
2021-03-22 19:58:07 +0530

    create scratch result page

--- src/pages/login/LoginForm.jsx
+++ src/pages/login/LoginForm.jsx
@@ -1,26 +1,33 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useHistory  } from 'react-router-dom';
 import PlayButton from "../../images/playbtn.svg";
 
 function LoginForm() {
+    const history = useHistory();
     const [userName,setUserName] = useState('');
+    const [error,setError] = useState(false);
     const [selectedDifficultyLevel,setSelectedDifficultyLevel] = useState('Easy');
-    const DifficultyArray = ['Easy','Medium','Hard']
+    const DifficultyArray = ['Easy','Medium','Hard'];
     const startGame = (e) => {
         e.preventDefault();
-        sessionStorage.setItem('USER',userName);
-        sessionStorage.setItem('DIFFICULY',selectedDifficultyLevel);
+        if(userName.length>0 && selectedDifficultyLevel){
+            setError(false)
+            sessionStorage.setItem('USER',userName);
+            sessionStorage.setItem('DIFFICULY',selectedDifficultyLevel);
+            history.push("/game");
+        }else{
+            setError(true)
+        }
     }
 
     return (
         <form id="loginform" onSubmit={startGame}>
+            {error && <p className="errorMsg">Name is Mandatory</p>}
             <input type="text" value={userName} onChange={e=>setUserName(e.target.value)} placeholder="ENTER YOUR NAME" id="name-input" />
             <select id="difficultyLevel" value={selectedDifficultyLevel} onChange={e=>setSelectedDifficultyLevel(e.target.value)}>
                 {DifficultyArray.map(val=>(<option key={val}>{val}</option>))}
             </select>
-            <button type="submit" onClick={startGame}>
-            <NavLink to="/game" className="loginBtn"> <span><img src={PlayButton} width="25px" height="25px" alt="playbutton" /></span><span className="start-game-text">Start Game</span> </NavLink>
-            </button>
+            <button type="submit" className="loginBtn"> <span><img src={PlayButton} width="25px" height="25px" alt="playbutton" /></span><span className="start-game-text">Start Game</span> </button>
         </form>
     )
 }
--- src/utilities/StopGame.jsx
+++ src/utilities/StopGame.jsx
@@ -5,7 +5,7 @@ function StopGame() {
     return (
         <div className="stopGame">
             <img src={Cross} width="25px" height="25px" alt="cross"/>
-            <h2>STOP GAME</h2>
+            <h2 className="stop-game-text">STOP GAME</h2>
         </div>
     )
 }
